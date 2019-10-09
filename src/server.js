055c920c070d66f28b0047e59216e6fecb1d9ae1
Siddharth <sidgroks@gmail.com>
2019-10-10 00:35:42 +0530

    dummy implementation

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
+'use strict';
+
+require('dotenv').config();
+
 const app = require('express')();
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -5,6 +9,20 @@ const boot = require('./boot');
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 3000;
+
+const httpServer = require('http').Server(app);
+const socketIO = require('socket.io')(httpServer);
+
+const socketStorage = require('./factories/socket-connections')();
+
+socketIO.on('connection', function (socket) {
+    if (socket
+        && socket.handshake
+        && socket.handshake.query
+        && socket.handshake.query.username) {
+        socketStorage.addConnection(socket.handshake.query.username, socket);
+    }
+});
 
 app.get('/ping', (req, res) => {
     res.send('pong');
@@ -18,13 +36,12 @@ boot.processBoot(app)
 
         app.use('/api', routes(app));
 
-        app.listen(PORT, () => {
+        httpServer.listen(PORT, () => {
             console.log("Listening on " + PORT);
         });
 
     })
-    .catch(err=>{throw err;});
-
+    .catch(err => { throw err; });
 
 
 module.exports = app;
